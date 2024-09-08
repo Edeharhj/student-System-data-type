c76a1c5fce6d6a6389b9e3ff676478c72400107b
Edeharhj <166995366+Edeharhj@users.noreply.github.com>
2024-09-08 21:26:23 +0800

    Update Main.java

--- For Student System Data Type/Main.java
+++ For Student System Data Type/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Create a new student
+        // Create a new student (anhi lang pag butang ug student)
         Student student1 = new Student("001", "Jharhede Vasquez", 19, "BSCS 1A", "Computer Studies", 1.5);
 
-        // Print student information
+        // Print the student information method
         System.out.println(student1.toString());
     }
 }
